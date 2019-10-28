e24fb52038328d69c250eed21ed627eac4a09c7e
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-10-28 13:08:56 +0100

    Create comprobar.sh

---
+++ packages/comprobar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Comprobar paquetes
+instalar_sino_esta() {
+	listar_paquete=$(dpkg-query -W -f='${Package}\n' | grep $paquete)
+	if [ "$listar_paquete" == "$paquete" ]
+	then
+	  echo "El paquete: $paquete, SI está instalado."
+	else
+	  echo -e "El paquete: $paquete, NO está instalado."
+	  # apt-get install $paquete -y 
+	fi
+}
+paquete="nano"
+instalar_sino_esta
