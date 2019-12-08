fcdd5414e2aa811c06f65638f64866b1f4bd454f
Bartolomé Vich Lozano <tolo@tovilo.es>
2019-12-09 00:43:27 +0100

    Update comprobar_servicio.sh

--- servicios/comprobar_servicio.sh
+++ servicios/comprobar_servicio.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 if [[ $(/etc/init.d/$1 status | grep 'active (running)' | cut -d" " -f5-6 ) = "active (running)" ]]; then
         echo "El servicio esta iniciado."
-	echo 1
 else
 	echo "El servicio no esta iniciado."
-        echo 0
 fi
